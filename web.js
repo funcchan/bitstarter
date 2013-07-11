178b6d35185f35b6ff799d7db8fb17db7cfa587a
Ubuntu <ubuntu@ip-172-31-10-182.us-west-2.compute.internal>
2013-07-11 21:59:59 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,19 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//set up the variablt to call the file
 var fs = require('fs');
+//return the content of file as buffer
 var content = fs.readFileSync('index.html');
-var buf = new Buffer(content.length);
-//for ( var i =0;i< content.length;i++){
-// buf[i]=content.charCodeAt(i);
-//}
-//console.log(buffer.toString('utf-8'));
 
-//buffer.write=(fs.readFileSync('index.html'), "utf-8");
-buf.write=(fs.readFileSync('index.html'), "utf-8");
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8'));
+  response.send(content.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
